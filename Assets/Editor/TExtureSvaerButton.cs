b95af6ee181f13b74956f0fc40b2e6ee827a1355
dem <dmitriy313.k@gmail.com>
2024-06-05 00:50:38 +0300

    rhyjuj

---
+++ Assets/Editor/TExtureSvaerButton.cs
@@ -0,0 +1,18 @@
+
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using Visual.Lighting;
+
+[CustomEditor(typeof(PointLight))]
+class DecalMeshHelperEditor : Editor {
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Print Render Texture"))
+        {
+            SaveTextureToFileUtility.SaveRenderTextureToFile(GameObject.Find("Light").GetComponent<PointLight>().renderTexture, "C:\\Users\\Дмитрий\\Desktop\\RenderTExture\\LightTexture.png" );
+        }
+            
+    }
+}
